f8896e5b3894309241d3a6b860285c93944c679b
Yankl-Peretz Blum <yakovblum@gmail.com>
2024-12-26 13:25:13 +0200

    Merge branch 'release/3.2.4' into yiddishpop

--- classes/HtmlHelper.php
+++ classes/HtmlHelper.php
@@ -32,6 +32,7 @@ trait HtmlHelper
     {
         libxml_use_internal_errors(true);
         $domDocument = new \DOMDocument();
+
         $html = "<page-toc>$markup</page-toc>";
         $domDocument->loadHTML(mb_encode_numericentity($html, [0x80, 0x10FFFF, 0, ~0], 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
         $domDocument->preserveWhiteSpace = true;
